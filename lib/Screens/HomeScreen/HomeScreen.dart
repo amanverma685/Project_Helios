0fd118c0f3c26197d6294a575233cfa8d1dc2c8d
amn <av36232@gmail.com>
2021-08-07 17:20:11 +0530

    Added New Screens

--- lib/Screens/HomeScreen/HomeScreen.dart
+++ lib/Screens/HomeScreen/HomeScreen.dart
@@ -10,6 +10,68 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+        child: Scaffold(
+      drawer: Container(
+        width: MediaQuery.of(context).size.width * 0.7,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(25),
+            bottomRight: Radius.circular(25),
+          ),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Center(
+                child: CircleAvatar(
+                  foregroundImage: AssetImage("assets/images/profile.jpg"),
+                  maxRadius: 100,
+                  minRadius: 40,
+                ),
+              ),
+            ),
+            Divider(
+              endIndent: 20,
+              color: Colors.blue,
+              indent: 20,
+              thickness: 2,
+            ),
+            Expanded(
+              flex: 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ListTile(
+                    title: Text("Aman"),
+                    onTap: () {},
+                    leading: Icon(Icons.home),
+                  ),
+                  ListTile(
+                    title: Text("Aman"),
+                    onTap: () {},
+                    leading: Icon(Icons.home),
+                    tileColor: Colors.orange,
+                  ),
+                  ListTile(
+                    title: Text("Aman"),
+                    onTap: () {},
+                    leading: Icon(Icons.home),
+                  ),
+                  ListTile(
+                    title: Text("Aman"),
+                    onTap: () {},
+                    leading: Icon(Icons.home),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(),
+    ));
   }
 }
--- lib/Screens/SplashScreen/Splash_Screen.dart
+++ lib/Screens/SplashScreen/Splash_Screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:helios/Screens/SIgnIn/GoogleSignIn/Google_Sign_In.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -10,6 +11,16 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: ElevatedButton(
+          child: Text("Get Started"),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => GoogleSignInScreen()));
+          },
+        ),
+      ),
+    );
   }
 }
